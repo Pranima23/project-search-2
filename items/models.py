b3c34abe2fac84c20149dae3b2c062e3e2a8c2a7
Pranima23 <kpranima23@gmail.com>
2020-08-26 09:59:38 +0545

    managed files and folders in a good manner, menu page displays items from database

--- items/models.py
+++ items/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Item(models.Model):
-    name = models.CharField(max_length = 50)
+    name = models.CharField(max_length=50)
     price = models.FloatField()
     about_item = models.TextField()
     id = models.AutoField(primary_key=True)
